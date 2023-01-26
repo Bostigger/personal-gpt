566dcbc8cbe523c1f210ca57763921bec4adcc1c
Bostigger <tricksteck@gmail.com>
2023-01-26 19:57:46 +0000

    first commit

---
+++ server/server.js
@@ -0,0 +1,44 @@
+import express from 'express'
+import * as dotenv from 'dotenv'
+import cors from 'cors'
+import { Configuration, OpenAIApi } from 'openai'
+
+dotenv.config()
+
+const configuration = new Configuration({
+    apiKey: process.env.OPEN_AI_KEY
+})
+const openai = new OpenAIApi(configuration)
+
+const app = express();
+app.use(cors());
+app.use(express.json())
+
+app.get('/',async (req,res)=>{
+    res.status(200).send({
+        message:'Hello from RemAi'
+    })
+})
+
+app.post('/',async (req,res)=>{
+    try{
+        const prompt = req.body.prompt;
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: `${prompt}`,
+            temperature: 0.7,
+            max_tokens: 3000,
+            top_p: 1,
+            frequency_penalty: 0.5,
+            presence_penalty: 0,
+        });
+        res.status(200).send({
+            bot:response.data.choices[0].text
+        })
+    }catch (e) {
+        console.log(e)
+        res.status(500).send(e)
+    }
+})
+
+app.listen(5000,()=>console.log('Server is running on http://localhost:5000'))
